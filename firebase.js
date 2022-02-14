63ffd320637879c410dfad18d94f1e196fae96d0
usama Abdul Razzaq <usamaabdulrazzaq@usamas-MBP>
2022-02-14 17:57:17 +0500

    half build without stripe

--- firebase.js
+++ firebase.js
@@ -1,13 +1,5 @@
 import firebase from "firebase";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAUf49740j_pxF5VjGkiduLYQxS0aSL6aY",
-  authDomain: "p0-84f14.firebaseapp.com",
-  projectId: "p0-84f14",
-  storageBucket: "p0-84f14.appspot.com",
-  messagingSenderId: "315251674602",
-  appId: "1:315251674602:web:2442af4d3a8858ef96b693",
-  measurementId: "G-KXY5JNKR1K",
-};
+const firebaseConfig = process.env.FIREBASE_CONFIG;
 
 // const app = firebase.app(firebaseConfig)
